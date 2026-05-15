1c42a9a8b84afb23476e9b3dab81b9e5e158b189
VictorSutac <156675989+VictorSutac@users.noreply.github.com>
2026-05-15 15:09:32 +0300

    Add explicit card rewrite

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
-import { HashRouter, Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import CardPage from "./pages/CardPage";
 
 function App() {
   return (
-    <HashRouter>
+    <BrowserRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/card" element={<CardPage />} />
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
-    </HashRouter>
+    </BrowserRouter>
   );
 }
 
